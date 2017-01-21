cb184639ae40e976d37f201ac7824a2d7b3eeabf
Cephas Reis <c3r1c3@nevermindonline.com>
2017-01-21 05:57:30 -0600

    Added Copyright notices to the top of all files.

--- headers/EditorWIdget.h
+++ headers/EditorWIdget.h
@@ -1,6 +1,19 @@
-//
-// Created by Colin Edwards on 12/31/16.
-//
+/*****************************************************************************
+Copyright (C) 2016-2017 by Colin Edwards.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*****************************************************************************/
 
 #ifndef OBS_STUDIO_EDITORDIALOG_H
 #define OBS_STUDIO_EDITORDIALOG_H
