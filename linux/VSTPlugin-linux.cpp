27de9a54019524e6bf54bfed9f39a0862a0ee0c6
Colin Edwards <colin@recursivepenguin.com>
2017-01-28 00:44:10 -0600

    Quick linux change

--- linux/VSTPlugin-linux.cpp
+++ linux/VSTPlugin-linux.cpp
@@ -23,27 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 AEffect* VSTPlugin::loadEffect() {
 	AEffect *plugin = nullptr;
 
-	// Why is this a (non-portable) wide char?
-	wchar_t *wpath = NULL;
-
-	// We need to convert the above wide char to a 'normal' char
-	// 4096 elements should be enough to contain it... I hope.
-	// We also initialize the array with NULLs to prevent issues
-	// down the road.
-	char charPath[4096] = {NULL};
-	uint32_t wcs_returnValue;
-	//os_utf8_to_wcs_ptr(pluginPath.c_str(), 0, &wpath);
-	wcs_returnValue = wcstombs(charPath, wpath, sizeof(charPath));
-
-	// Now check for errors in the conversion before trying to open
-	// the file/path.
-	if (wcs_returnValue < 4) {
-		wprintf(L"Path conversion error from '%s'", wpath);
-		printf(", to '%s'\n", charPath);
-		return nullptr;
-	}
-
-	soHandle = os_dlopen(charPath);
+	soHandle = dlopen(pluginPath.c_str(), RTLD_LAZY);
 	bfree(wpath);
 	bfree(charPath);
 	if(soHandle == nullptr) {
