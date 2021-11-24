a6e2326e70eda0984bf8699226206dc6a5103d43
wangshaohui <wang.shaohui@navercorp.com>
2021-11-24 11:06:15 +0800

    clang format

--- headers/VSTPlugin.h
+++ headers/VSTPlugin.h
@@ -45,7 +45,7 @@ class VSTPlugin : public QObject {
 	float **inputs;
 	float **outputs;
 
-	EditorWidget *editorWidget = nullptr;
+	EditorWidget *editorWidget  = nullptr;
 	bool          editorOpenned = false;
 
 	AEffect *loadEffect();
